c652dc080aadf960a86eae59c463795b13aa0bb1
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-16 23:30:29 +0200

    Fix workflow indentation and remove self-require in calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,11 +7,15 @@ const LONG_TERM_DISCOUNT = 50;
 function calculateRentalCost(numberOfDays) {
   const basePrice = numberOfDays * DAILY_RATE;
 
-  if (numberOfDays >= LONG_TERM_DAYS) return basePrice - LONG_TERM_DISCOUNT;
-  if (numberOfDays >= MID_TERM_DAYS) return basePrice - MID_TERM_DISCOUNT;
+  if (numberOfDays >= LONG_TERM_DAYS) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (numberOfDays >= MID_TERM_DAYS) {
+    return basePrice - MID_TERM_DISCOUNT;
+  }
 
   return basePrice;
 }
 
 module.exports = calculateRentalCost;
-
